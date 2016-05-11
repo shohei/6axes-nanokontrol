496f58fc88364feef8d6d9d87c29366e3eba3e42
Shohei Aoki <shoaok@gmail.com>
2016-05-11 15:38:00 +0900

    modify serial sample

--- serial-sample.js
+++ serial-sample.js
@@ -1,25 +1,30 @@
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort
 var port = new SerialPort("/dev/tty.usbmodem1411", {
-  baudrate: 9600,
+  baudrate: 115200,
   parser: serialport.parsers.readline('\n')
   }
 );
 
-tel = "23333320"
-msg="hello world"
-sentence = "{\"tel\":\""+tel+"\",\"message\":\""+msg+"\"}"
+dist1 = "129"
+dist2 = "22"
+dist3 = "19"
+dist4 = "122"
+dist5 = "9"
+dist6 = "2"
+sentence = "{\"dist1\":\""+dist1+"\",\"dist2\":\""+dist2+"\",\"dist3\":\""+dist3+"\",\"dist4\":\""+dist4+"\",\"dist5\":\""+dist5+"\",\"dist6\":\""+dist6+"\"}"
 
 port.on('open', function () {
-  setTimeout(sendMessage,5000);
+  console.log("hold on. operation starts soon.");
+  setInterval(sendMessage,3000);
 });
 
 port.on('data', function (data) {
-  console.log('Receied: ' + data);
+  console.log('Received: ' + data);
 });
 
 function sendMessage(){
-  console.log('firing up.');
+  console.log('firing it up!');
   port.write(sentence, function(err,bytesWritten){
     if(err){
       return console.log('Error: ',err.message);
@@ -29,9 +34,3 @@ function sendMessage(){
 }
 
 
-// port.write('main screen turn on', function(err, bytesWritten) {
-// port.write(sentence, function(err, bytesWritten) {
-//   if (err) {
-//     return console.log('Error: ', err.message);
-//   }
-// console.log(bytesWritten, 'bytes written');
